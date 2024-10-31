269dd728de7191319e90d39afa80074121918da5
Aadil Hussain <127195496+Aadil-hussain-786@users.noreply.github.com>
2024-10-31 07:16:41 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -56,7 +56,7 @@ const Hero = () => {
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <p className="text-center text-black md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          <p className="text-center text-white md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
              السلام عليكم I&apos;m Aadil Hussain, a Machine Learning and Deep Learning Developer.
           </p>
 
