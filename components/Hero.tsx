3246e254aef7409d51775e7c35a886b569ae5058
Aadil Hussain <127195496+Aadil-hussain-786@users.noreply.github.com>
2024-10-31 07:24:08 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,14 +29,13 @@ const Hero = () => {
        *  0.2 to 0.03
        */}
       <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+        className="h-screen w-full bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
         {/* Radial gradient for the container to give a faded look */}
         <div
           // chnage the bg to bg-black-100, so it matches the bg color and will blend in
-          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+          className="absolute pointer-events-none inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
       </div>
 
@@ -56,7 +55,7 @@ const Hero = () => {
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <p className="text-center text-white md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          <p className="text-center text-black md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
              السلام عليكم I&apos;m Aadil Hussain, a Machine Learning and Deep Learning Developer.
           </p>
 
